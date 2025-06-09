68b25c23f3e352110a6391b2b21aa12d998a4d1b
contato.geovanigaldino@gmail.com <164828104+geovanigaldino@users.noreply.github.com>
2025-06-08 21:47:03 -0300

    Adicionado estrutura inicial do projeto

---
+++ src/main/java/br/com/LiterTech/LiterTechApplication.java
@@ -0,0 +1,13 @@
+package br.com.LiterTech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiterTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiterTechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/LiterTech/LiterTechApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.LiterTech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiterTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
